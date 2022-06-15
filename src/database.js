c28370db8d55939f27e71e17da2dea93eb511185
estefania <estefaniaaren0@gmail.com>
2022-06-14 23:02:52 -0500

    bd

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-URL=('mongodb://localhost/BdAdopciones');
+// URL=('mongodb://localhost/bdmascotas');
 
-//URL=('mongodb+srv://LauCaicedo:diaz18210@cluster0.gyn1k.mongodb.net/BaseDeDatosAdopciones?retryWrites=true&w=majority');
+URL=('mongodb+srv://LauCaicedo:diaz18210@cluster0.gyn1k.mongodb.net/BaseDeDatosAdopciones?retryWrites=true&w=majority');
 
 
 mongoose
