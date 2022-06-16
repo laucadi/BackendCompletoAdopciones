4a985f9a116734b3f55e073e58a33b58823c9469
estefania <estefaniaaren0@gmail.com>
2022-06-16 14:51:19 -0500

    actualizar estructuroa de puerto

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const cors = require('cors');
-const bodyParser = require('body-parser')
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 require("./database");
 
-app.set('Port', process.env.PORT || 5000);
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: true}));
+const port = process.env.PORT || 8000;
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({origen: '*'}));
+app.use(cors({ origen: "*" }));
 
-app.listen(app.get("Port"), () => {
-    console.log("El servidor esta escuchando por el puerto: ", app.get("Port"));
-  });
+app.listen(port, function() {
+  console.log("La aplicación esta escuchando en el puerto " + port);
+});
 
-  app.use("/usuarios", require('./router/usuarios.route'));
-  app.use("/mascotas", require('./router/mascotas.route'));
+app.use("/usuarios", require("./router/usuarios.route"));
+app.use("/mascotas", require("./router/mascotas.route"));
