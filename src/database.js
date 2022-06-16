a5e8df84f24c34c854423e599c09ff0e75f58388
estefania <estefaniaaren0@gmail.com>
2022-06-15 23:12:22 -0500

    pequeño cambio

--- src/database.js
+++ src/database.js
@@ -1,13 +1,6 @@
 const mongoose = require("mongoose");
-<<<<<<< HEAD
-
-//URL = "mongodb://localhost/BdAdopciones";
-
-//URL = "mongodb://localhost/BdAdopciones";
-=======
 // URL = "mongodb://localhost/BdAdopciones";
 
->>>>>>> f19d65173bb76ca394c9200cfbc44d4dbb441e91
 URL =
   "mongodb+srv://LauCaicedo:diaz18210@cluster0.gyn1k.mongodb.net/BaseDeDatosAdopciones?retryWrites=true&w=majority";
 
